8f99af73a6cb5afbcead8de68b2693413dd46758
vastness <1916286435@qq.com>
2022-03-27 22:18:23 +0800

    update

--- init/unbuntu_init.sh
+++ init/unbuntu_init.sh
@@ -19,7 +19,10 @@ if [ $sources == 'y' ]; then
 	apt update
 fi
 echo '=======================install softwares======================='
-softwares=(fd yadm neovim ranger fzf autojump git zsh)
+add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:lazygit-team/release
+sudo apt update
+softwares=(software-properties-common python3.9 golang fd-find ruby-full bat make yadm lazygit neovim ranger fzf autojump git zsh ripgrep htop)
 for software in "${softwares[@]}"; do
 	software_cli=$(whereis "$software")
 	if [ "${software_cli}" != "$software:" ]; then
@@ -29,11 +32,27 @@ for software in "${softwares[@]}"; do
 		apt install "$software" -y
 	fi
 done
+
+curl -sS https://starship.rs/install.sh | sh
+go get github.com/jesseduffield/lazydocker
+
+echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
+wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+apt update
+apt install docker-ctop
+
+gem install colorls
+
+# zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 echo '=======================config git======================='
 git config --global user.name vastness
 git config --global user.email 1916286435@qq.com
 git clone https://gitee.com/yin_haoyu/nvim.git /home/vastness/.config/nvim
 git clone https://gitee.com/yin_haoyu/config.git /home/vastness/.config
+git clone git://github.com/sjl/oh-my-zsh.git ~/.oh-my-zsh
+yadm clone  https://gitee.com/yin_haoyu/yadm.git
 echo '=======================config zsh======================='
 chsh -s /usr/bin/zsh
 
